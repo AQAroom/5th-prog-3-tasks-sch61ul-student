bfbea2b7f3b4ca4bcd65815b3e8fc886cd39e91d
sch61ul-student <sch61ul@gmail.com>
2025-12-16 10:18:03 +0400

    Create task_01.py

---
+++ task_01.py
@@ -0,0 +1,18 @@
+a = int(input())
+z = input()
+b = int(input())
+if z == "+":
+    print(a +  b)
+elif z == "-":
+    print(a - b)
+elif z == "*":
+    print(a * b)
+elif z == "/":
+    if b == 0:
+        print("error")
+    else:
+        print(a / b)
+elif z == "^":
+    print(a  ** b)
+else:
+    print( "Неизвестная операция" )
